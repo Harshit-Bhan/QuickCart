2a7383bd7e2687836f9571c26e29c1ec40818080
Harshit-Bhan <harshitbhan2004@gmail.com>
2025-12-18 17:46:54 +0530

    Inngest Endpoints Created

---
+++ app/api/inngest/route.js
@@ -0,0 +1,12 @@
+import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/app/config/inngest";
+import { serve } from "inngest/next";
+// import { inngest , syncUserCreation , syncUserDeletion , syncUserUpdation } from 
+
+export const { GET , POST , PUT } = serve({
+    client: inngest,
+    functions: [
+        syncUserCreation,
+        syncUserDeletion,
+        syncUserUpdation
+    ],
+})
